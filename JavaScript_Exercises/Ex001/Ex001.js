dd76723baab421e6e76fa6098f7e4771cf876485
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-08-04 10:26:25 -0300

    up

---
+++ JavaScript_Exercises/Ex001/Ex001.js
@@ -0,0 +1,8 @@
+let nome = window.prompt('Digite Seu Nome: ')
+let idade = window.prompt('Digite Sua Idade: ')
+let sexo = window.prompt('Digite seu Sexo [F/M]: ')
+let endereco = window.prompt('Digite Seu endereço: ')
+let telefone = window.prompt('Digite Seu numero de telefone: ')
+
+console.log(`Seu nome é ${nome}, Sua idade é ${idade}, e voce é do sexo ${sexo}!!`)
+console.log(`Seu endereço atual é ${endereco}, e o seu telefone pra contato é o ${telefone}!!`)
---
+++ JavaScript_Exercises/Ex002/Ex002.js
@@ -0,0 +1,10 @@
+let nome = window.prompt('Digite Seu nome: ')
+let nota_1 = window.prompt('Digite a Nota do Primeiro Bimestre: ')
+let nota_2 = window.prompt('Digite a Nota do Segundo Bimestre: ')
+let nota_3 = window.prompt('Digite a Nota do Terceiro Bimestre: ')
+let nota_4 = window.prompt('Digite a Nota do Quarto Bimestre: ')
+
+console.log(`A nota do Primeiro Bimestre é: ${nota_1}`)
+console.log(`A nota do Segundo Bimestre é: ${nota_2}`)
+console.log(`A nota do Terceiro Bimestre é: ${nota_3}`)
+console.log(`A nota do Quarto Bimestre é: ${nota_4}`)
