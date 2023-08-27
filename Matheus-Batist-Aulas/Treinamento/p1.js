a2074d779a4a6e6389eba10eef51d7945c25bcf4
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-08-27 01:18:44 -0400

    up

---
+++ Matheus-Batist-Aulas/Treinamento/p1.js
@@ -0,0 +1,18 @@
+var minhaVariavel = "Olá, Mundo!!"
+console.log(minhaVariavel)
+
+// Variaveis e tipos de dados!
+
+var meuNumero = 10
+console.log(meuNumero) // Só imprime o numero na tela (console)
+console.log(meuNumero + 5) // Concatenacao
+console.log(typeof meuNumero) // typeof = mostre o tipo do dado da variavel
+
+var booleano = true // Verdadeira e falso
+var meuObjeto = {} // Um objeto
+var meuArray = [] // Uma lista
+var meuNull = null // Recebe um valor nulo para posteriormente ser atribuido algo
+var meuUndefined = undefined // esse valor é gerado quando n se é atribuido um valor a variavel
+
+console.log(booleano)
+console.log(meuObjet)
