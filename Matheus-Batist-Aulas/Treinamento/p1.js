6559f12e65dca41b8466f3031f0769a24a8b04d1
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-08-28 10:58:54 -0400

    up

--- Matheus-Batist-Aulas/Treinamento/p1.js
+++ Matheus-Batist-Aulas/Treinamento/p1.js
@@ -128,6 +128,161 @@ switch(fruta){
 
 // contador, condicao limite, Incremento
 
+for(let i = 0; i<5; i=i+1){
+    console.log(`O valor de i é ${i}`)
+}
+
+
+// While(Enquanto)
+
+let s = 0 
+
+while (s < 5){
+    console.log(`O valor de s é: ${s}`)
+}
+
+// do while
+
+let j = 0
+
+do{
+    console.log(`O valor de j é de: ${j}`)
+}while(j < 5)
+
+// Funcoes
+
+function comprimentar(){
+    console.log("OLá!!")
+}
+
+
+// Invocacao da funcao:
+
+comprimentar()
+
+//Escopo de variaveis
+
+let cor = "Azul" // Isso é uma variavel Global
+
+function mostrar_cor(){
+    let cor = "Verde" // Isso é uma variavel local (dentro do escopo)
+}
+
+
+// Hosting = Içamento
+
+testeHoisting()
+
+function testeHoisting(){
+    console.log("Deu Certo!!")
+}
+
+
+// Arrow Function
+
+const testeSrrow = (/*Paramentros*/) = console.log("Isso tambem é uma funcao")
+
+
+// truthy e Falsy
+
+const minhaVariavel1 = ''
+const minhaVariavel2 = 'Alguma Coisa'
+
+if(minhaVariavel1){
+    console.log("É Verdadeiro!")
+}else{
+    console.log("É Falso!!")
+}
+
+
+if(minhaVariavel2){
+    console.log("É Verdadeiro!! 2")
+}else{
+    console.log("É Falso!! 2")
+}
+
+
+// Arrays
+
+const arrays = ['Elemento1','Elemento2','Elemento3','Elemento4','Elemento5']
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(numeros)
+
+console.log(numeros[0])
+
+console.log(numero[1])
+console.log(numero[2])
+console.log(numero[3])
+console.log(numero[4])
+console.log(numero[5])
+console.log(numero[6])
+console.log(numero[7])
+console.log(numero[8])
+console.log(numero[9])
+console.log(numero[10])
+
+
+// Para adicionar item a lista(no final)
+
+numeros.push(11)
+console.log(numeros)
+
+// Para retirar um item da lista (no final)
+
+numeros.pop(11)
+console.log(numeros)
+
+// Strings
+
+// Concatenacao
+
+const minhaStringNova = "Olá Mundo!"
+const minhaStringNova2 = minhaStringNova + "Como Voce Esta!"
+console.log(minhaStringNova2)
+
+const minhaStringNova3 = `${minhaStringNova2} Como Voce Esta?`
+console.log(minhaStringNova3)
+
+console.log(minhaStringNova3.length)
+console.log(minhaStringNova3[5])
+
+console.log(minhaStringNova.toLocaleUpperCase())
+
+console.log(minhaStringNova.toLocaleLowerCase())
+
+// Data e Hora
+
+const agora = Date()
+console.log(agora)
+
+const natal = new Date(2023,11,25)
+console.log(natal)
+
+//  math
+
+console.log(Math.PI)
+console.log(Math.round(3.6))
+console.log(Math.sqrt(16))
+console.log(Math.pow(2,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
