4890dc4a512f04d18a059a822291302e24525758
Arthur Brum Xavier <arthurbrum0ficial22@gmail.com>
2023-08-30 01:15:11 -0400

    up

--- Matheus-Batist-Aulas/Aula-4Hrs/Exercicios/ex003.js
+++ Matheus-Batist-Aulas/Aula-4Hrs/Exercicios/ex003.js
@@ -2,7 +2,7 @@
 // Manipulacao De Strings
 // Dado Uma String "JavaScript é Incrivel" Escreva um codigo que conte quantas caracteres a string tem e quantas palavras existem na frase
 
-
+let c = ['Js é incrivel']
 
 
 
