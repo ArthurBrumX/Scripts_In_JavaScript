d6b90802d8ab5bd7c065559c11cc79f2b7355de9
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-01 12:35:13 -0300

    up

---
+++ Matheus-Batist-Aulas/Treinamento/p2.js
@@ -0,0 +1,24 @@
+// Fundamentos
+
+// Var nome = valor
+
+var minhaVariavel = "Olá, Mundo!" // Declaracao de variavel
+console.log(minhaVariavel) // Imprimindo na area de console)
+
+/*
+para comentar 
+em
+mais
+de uma
+linha
+*/
+
+// Comentar muito o codigo significa que o codigo esta ma escrito
+
+//-----------------------------------------------------------------------------------------------
+
+// Variaveis e tipos de dados!
+
+var meuNumero = 10
+console.log(meuNumero)
+console.log(meuNumero + 5)
